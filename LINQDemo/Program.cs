b1b176414c5a9052598901d69e47c1a1948e0aba
Arif Shahriar <arifanik28@gmail.com>
2026-01-24 23:31:07 -0700

    Added AggregateBy and Index method examples

--- LINQDemo/Program.cs
+++ LINQDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using LINQMethods.Models;
+﻿using LINQMethods.Helpers;
+using LINQMethods.Models;
 
 namespace LINQDemo;
 
@@ -16,7 +17,9 @@ class Program
     static void Main(string[] args)
     {
         // CountByExample();
-        AggregateByExample();
+        // AggregateByExample();
+        // AggregateByExampleWithoutCustomAccumulator();
+        IndexExample();
     }
         
     /// <summary>
@@ -35,7 +38,33 @@ class Program
             
         Console.WriteLine("\nAverage Probability by Bucket:");
         foreach (var (bucket, averageAccumulator) in averageByBucket)
+        {
             Console.WriteLine($"{bucket}: {averageAccumulator.Value:P0}");
+        }
+    }
+    
+    /// <summary>
+    /// Demonstrates the functionality of the AggregateBy LINQ method without using a custom accumulator.
+    /// </summary>
+    private static void AggregateByExampleWithoutCustomAccumulator()
+    {
+        double[] probabilities = [0.05, 0.12, 0.31, 0.40, 0.55, 0.66, 0.71, 0.88, 0.97];
+        
+        var averageByBucket = probabilities
+            .Select(static probability => (probability: probability, bucket: Bucket(probability)))
+            .AggregateBy(
+                keySelector: static bin => bin.bucket,
+                seedSelector: static _ => (Sum: 0.0, Count: 0),
+                func: static (accumulator, bin) => (accumulator.Sum + bin.probability, accumulator.Count + 1));
+        
+        Console.WriteLine("\nAverage Probability by Bucket:");
+        foreach (var (bucket, accumulator) in averageByBucket)
+        {
+            var average = accumulator.Count == 0 
+                ? 0
+                : accumulator.Sum / accumulator.Count;
+            Console.WriteLine($"{bucket}: {average:P0}");
+        }
     }
     
     /// <summary>
@@ -59,6 +88,21 @@ class Program
             .CountByMethod.CalculateTermFrequency(Document);
 
         foreach (var (word, frequency) in wordFrequency)
+        {
             Console.WriteLine($"{word}: {frequency}");
+        }
+    }
+    
+    /// <summary>
+    /// Demonstrates the functionality of the Index LINQ method
+    /// </summary>
+    private static void IndexExample()
+    {
+        var words= DocumentHelper.SplitWords(Document);
+
+        foreach (var (index, word) in words.Index())
+        {
+            Console.WriteLine($"{index + 1}: {word}");
+        }
     }
 }
---
+++ LINQMethods/Helpers/DocumentHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LINQMethods.Helpers;
+
+public static partial class DocumentHelper
+{
+    [GeneratedRegex(@"[\w]+")] 
+    private static partial Regex WordMatcher();
+    
+    /// <summary>
+    /// Splits the input document into individual words.
+    /// </summary>
+    /// <param name="document"></param>
+    /// <returns></returns>
+    public static IEnumerable<string> SplitWords(string document) =>
+        WordMatcher().Matches(document).Select(static match => match.Value);
+}
